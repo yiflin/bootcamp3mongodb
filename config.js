52664a5c11363d5d5179d91cc962fcfe10d448ee
prhodesbrown <philipparhodes@hotmail.com>
2019-01-27 23:32:32 -0500

    Bootcamp 3

---
+++ config.js
@@ -0,0 +1,11 @@
+//This file holds any configuration variables we may need 
+//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+
+
+module.exports = {
+  db: {
+    uri: '', //place the URI of your mongo database here.
+  }
+};
+
+/* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() */
